81ad24a546b3afa0d946fb6850fe4af57d00e19e
anson <aponty@gmail.com>
2018-03-01 15:18:13 -0500

    s

--- application.py
+++ application.py
@@ -1,12 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
